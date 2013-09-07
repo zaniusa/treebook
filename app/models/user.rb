ca7277c705bbff4e28773f61633d7c29edc257a1
Claudio Zani <zani69@me.com>
2013-09-06 21:59:16 -0400

    forms updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  has_many :statuses
+
+  def full_name
+  	first_name + " " + last_name
+  end
 end
