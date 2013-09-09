1711b307b39b8a70633025f1ffdc6a7996779a56
Claudio Zani <zani69@me.com>
2013-09-09 00:10:49 -0400

    Styled my message class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	def flash_class(type)
+		case type
+		when :alert
+			"alert-error"
+		when :notice
+			"alert-success"
+		else
+			"alert-success"
+		end
+	end
+	
 end
